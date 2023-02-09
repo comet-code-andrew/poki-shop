24cd9b22fba26d3a27d7593f6be2bcc3f6c32c7a
Andrew Heyer <andrewbheyer@gmail.com>
2023-02-09 10:13:45 -0800

    scaffolding and Sass added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss'
 
-function App() {
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: "Chris's Plushies",
+      subtitle: 'Gotta catch em all!'
+    },
+    {
+      id: 2,
+      title: "Los's rootin tootin cowboy hides",
+      subtitle: 'Gotta catch em all!'
+    },
+    {
+      id: 3,
+      title: 'Tavios Gambling Wares',
+      subtitle: 'Gotta catch em all!'
+    },
+    {
+      id: 4,
+      title: 'Kevins Ninja Arsenal',
+      subtitle: 'Gotta catch em all!'
+    },
+    {
+      id: 5,
+      title: 'Juans Marvel Comics',
+      subtitle: 'Gotta catch em all!'
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className="categories-container">
+        {categories.map((category) => (
+          <div key={category.id} className="category-container">
+            {/*img*/}
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>{category.subtitle}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+    </>
   );
 }
 
