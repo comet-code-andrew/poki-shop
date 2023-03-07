8d8499ca97cc335588f553dc7e69c2a3a4402150
Andrew Heyer <andrewbheyer@gmail.com>
2023-03-07 14:17:06 -0800

    category async fetch changed to thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,19 +5,14 @@ import Category from "../category/category.component";
 
 import './shop.styles.scss';
 import {useEffect} from "react";
-import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
-import {setCategories} from "../../store/categories/category.action";
+import {fetchCategoriesAsync} from "../../store/categories/category.action";
 import {useDispatch} from "react-redux";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    }
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, [])
 
 
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import {persistStore, persistReducer} from "redux-persist";
 import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger';
 import {loggerMiddleware} from "./middleware/logger";
+import thunk from 'redux-thunk'
 
 import { rootReducer } from "./root-reducer";
 // Root reducer - combination of all out reducers
@@ -16,7 +17,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== 'production' && loggerMiddleware].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && loggerMiddleware, thunk].filter(Boolean);
 
 const composeEnhancer =
   ( process.env.NODE_ENV !== 'production' &&
